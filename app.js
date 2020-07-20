9eaafecd51e98f6c9a76e9a835455f731c77ecad
shaikhaalammmari <shaikhaala@live.com>
2020-07-20 19:38:34 +0300

    done

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require("express");
 let products = require("./products");
 const cors = require("cors");
+const bodyParser = require("body-parser");
 
 const app = express();
+app.use(bodyParser.json());
 app.use(cors());
 
 app.get("/", (req, res) => {
@@ -24,6 +26,13 @@ app.delete("/products/:productId", (req, res) => {
   } else {
     res.status(404).json({ message: "product is not found " });
   }
+});
+
+app.post("/products", (req, res) => {
+  const id = products[products.length - 1].id + 1;
+  const newProduct = { ...req.body, id };
+  products.push(newProduct);
+  res.status(201).json(newProduct);
 });
 
 app.listen(8000, () => {
--- products.js
+++ products.js
@@ -2,6 +2,7 @@ const products = [
   {
     id: 1,
     name: "overlapback dress ",
+    slug: "overlapback-dress",
     price: 25,
     image:
       "https://www.doodlage.in/wp-content/uploads/2019/11/08-11-19-BAYB1-02881.jpg",
@@ -10,6 +11,7 @@ const products = [
   {
     id: 2,
     name: "Orange Recycled Material Bag",
+    slug: "orange-recycled-material-bag",
     price: 40,
     image:
       "https://www.raeburndesign.co.uk/media/catalog/product/cache/189fe8db56a91dc68672618a725a226d/r/u/ru74004rexc_c800_a.jpg",
@@ -18,6 +20,7 @@ const products = [
   {
     id: 3,
     name: "cap",
+    slug: "cap",
     price: 12,
     image:
       "https://ecoalf.com/13018-thickbox_default/friendly-url-autogeneration-failed.jpg",
@@ -26,6 +29,7 @@ const products = [
   {
     id: 4,
     name: "pangia T-Shirt pink",
+    slug: "pangia-t-shirt-pink",
     price: 35,
     image:
       "https://cdn.shopify.com/s/files/1/0035/1309/0115/products/Organic-Cotton-T-Shirt-Rose-Pink-Front.jpg?v=1594393174",
@@ -36,6 +40,7 @@ const products = [
   {
     id: 5,
     name: "There is No Planet B T-Shirt",
+    slug: "there-is-no-planet-b-t-shirt",
     price: 20,
     image: "https://ecoalf.com/14351-large_default/because-washed-sweater.jpg",
     description: "Put on this shirt made with 100% organic cotton. ",
