b39db98eedcb77c6f103444a2939d8f5cb82b2d9
shaikhaalammmari <shaikhaala@live.com>
2020-07-21 17:56:12 +0300

    done here

--- app.js
+++ app.js
@@ -2,8 +2,7 @@ const express = require("express");
 let products = require("./products");
 const cors = require("cors");
 const bodyParser = require("body-parser");
-const slugfiy = require ("slugify")
-
+const slugify = require("slugify");
 
 const app = express();
 app.use(bodyParser.json());
@@ -18,6 +17,14 @@ app.get("/products", (req, res) => {
   res.json(products);
 });
 
+app.post("/products", (req, res) => {
+  const id = products[products.length - 1].id + 1;
+  const slug = slugify(req.body.name, (lower = true));
+  const newProduct = { ...req.body, id };
+  products.push(newProduct);
+  res.status(201).json(newProduct);
+});
+
 app.delete("/products/:productId", (req, res) => {
   const { productId } = req.params;
   const existProduct = products.find((product) => product.id === +productId);
@@ -30,12 +37,16 @@ app.delete("/products/:productId", (req, res) => {
   }
 });
 
-app.post("/products", (req, res) => {
-  const id = products[products.length - 1].id + 1;
-  const newProduct = { ...req.body, id };
-  const slug = slugify (req.body.name, {lower= true})
-  products.push(newProduct);
-  res.status(201).json(newProduct);
+app.put("/products/:productId", (req, res) => {
+  const { productId } = req.params;
+  const existProduct = products.find((product) => product.id === +productId);
+
+  if (existProduct) {
+    for (const key in req.body) existProduct[key] = req.body[key];
+    res.status(204).end();
+  } else {
+    res.status(404).json({ message: "product is not found" });
+  }
 });
 
 app.listen(8000, () => {
