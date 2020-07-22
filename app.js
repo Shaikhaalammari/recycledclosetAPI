741d0cc599dd953c20b406b66b30356d19826ed0
shaikhaalammmari <shaikhaala@live.com>
2020-07-22 20:05:53 +0300

    connection to db is successful

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 //route
 const productRoute = require("./routes/products");
-
+const db = require("./db/db");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 
@@ -12,6 +12,16 @@ app.use(cors());
 
 app.use("/products", productRoute);
 
-app.listen(8000, () => {
-  console.log("the app is running on localhost:8000");
-});
+const run = async () => {
+  try {
+    await db.authenticate();
+    console.log("Connection to the database successful!");
+  } catch (error) {
+    console.error("Error connecting to the database: ", error);
+  }
+  app.listen(8000, () => {
+    console.log("the app is running on localhost:8000");
+  });
+};
+
+run();
